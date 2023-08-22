55d6dc7abbba1929ed0762a72891d0a65f3e50ed
Andrew Sturman <andrewdavidsturman@gmail.com>
2023-08-22 16:40:13 +1000

    Added frontend handling of first app user case
    If a user is the first one to use the application, they can't be
    compared to any other users. The app was previously crashing
    in this case, so lets make it clear to the user what's happening.
    
    Also removed the example HomePage test now we have some
    actual frontend tests.

--- client/src/components/SimilarTypists.tsx
+++ client/src/components/SimilarTypists.tsx
@@ -4,10 +4,16 @@ interface SimilarTypistsProps {
 
 function SimilarTypists({ names }: SimilarTypistsProps) {
   const similarTypistNames = names.map(name => <li key={name}>{name}</li>)
+
+  const headingText =
+    names.length > 0
+      ? "Your typing is most similar to:"
+      : "No similar people could be found. You are likely the first person using this app! Come back soon, or try again under a different name."
+
   return (
     <>
-      <h2>Your typing is most similar to</h2>
-      <ol>{similarTypistNames}</ol>
+      <h2 data-testid="similar-names-heading">{headingText}</h2>
+      <ol data-testid="similar-names-list">{similarTypistNames}</ol>
     </>
   )
 }
