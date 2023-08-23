2fdac6cf8962476bb14621037a63c866f1574564
Andrew Sturman <andrewdavidsturman@gmail.com>
2023-08-23 15:56:29 +1000

    Switched to using median
    Previous speed results were being merged by summing and then multiplying them by two.
    Switching to median means outliers will have less impact, and a results should get more
    accurate over time.

--- server/UserProfile.py
+++ server/UserProfile.py
@@ -1,8 +1,12 @@
+import statistics
+
+
 class UserProfile:
     def __init__(self, username: str, milliseconds_per_character: float):
         self.username = username
-        self.milliseconds_per_character = milliseconds_per_character
+        self.median_milliseconds_per_character = milliseconds_per_character
+        self.milliseconds_per_character_previous_tests = [milliseconds_per_character]
 
     def update_with_new_test(self, new_profile: 'UserProfile'):
-        self.milliseconds_per_character = (self.milliseconds_per_character + new_profile.milliseconds_per_character) / 2
-
+        self.milliseconds_per_character_previous_tests.append(new_profile.median_milliseconds_per_character)
+        self.median_milliseconds_per_character = statistics.median(self.milliseconds_per_character_previous_tests)
