633066b4434e46e8ffb4969b03d8b3388feff8ad
Andrew Sturman <andrewdavidsturman@gmail.com>
2023-08-23 13:33:30 +1000

    Allow prod frontend to connect to hosted server

--- client/src/api/axios-config.ts
+++ client/src/api/axios-config.ts
@@ -1,6 +1,9 @@
 import axios from "axios"
 
-let baseURL = "http://localhost:5000"
+let baseURL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:80"
+    : "keystroke-dynamics-server-lb-119970644.ap-southeast-2.elb.amazonaws.com"
 const axiosInstance = axios.create({
   baseURL,
 })
