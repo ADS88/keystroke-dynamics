94d56e7a9eb7e2f71c6ec29d91e9419155649570
Andrew Sturman <andrewdavidsturman@gmail.com>
2023-08-23 17:10:17 +1000

    Improved frontend ux and validation
    Moved sentence above name as most people start typing
    the sentence first anyway. Also made sentence autofocus
    and disallowed submitting empty name or sentence.

--- client/src/components/SentenceInput.tsx
+++ client/src/components/SentenceInput.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useRef, useState } from "react"
+
 interface SentenceInputProps {
   sentence: string
   username: string
@@ -13,21 +15,36 @@ function SentenceInput({
   handleSentenceKeyChange,
   handleNameChange,
 }: SentenceInputProps) {
+  const sentenceInputRef = useRef<HTMLTextAreaElement>(null)
+  const [typedSentence, setTypedSentence] = useState("")
+  useEffect(() => sentenceInputRef.current?.focus(), [])
+
+  const handleTypedSentenceChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setTypedSentence(event.target.value)
+  }
+
+  const submitDisabled = username.length === 0 || typedSentence.length === 0
+
   return (
     <>
       <h1>{sentence}</h1>
+      <label htmlFor="sentence">Sentence</label>
+      <textarea
+        id="sentence"
+        name="sentence"
+        onKeyDown={handleSentenceKeyChange}
+        onKeyUp={handleSentenceKeyChange}
+        ref={sentenceInputRef}
+        value={typedSentence}
+        onChange={handleTypedSentenceChange}
+      ></textarea>
       <form>
         <label htmlFor="name">Name</label>
         <input type="text" name="name" id="name" value={username} onChange={handleNameChange} />
-        <label htmlFor="sentence">Sentence</label>
-        <textarea
-          id="sentence"
-          name="sentence"
-          onKeyDown={handleSentenceKeyChange}
-          onKeyUp={handleSentenceKeyChange}
-        ></textarea>
       </form>
-      <button onClick={handleDoneClicked}>Done</button>
+      <button onClick={handleDoneClicked} disabled={submitDisabled}>
+        Done
+      </button>
     </>
   )
 }
